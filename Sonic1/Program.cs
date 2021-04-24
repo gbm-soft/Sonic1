becf553a76a7c52ff4725f71ae82a6e023ceb428
megabspam <megabspam@yandex.ru>
2021-04-24 20:47:38 +0700

    fix

--- Sonic1/Program.cs
+++ Sonic1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Sonic1
     {
         static void Main(string[] args)
         {
+            StreamReader fs1 = new StreamReader(args[0]);
+            StreamWriter result = new StreamWriter(args[0] + ".email");
+
+            string s = "";
+            while (s != null)
+            {
+                s = fs1.ReadLine();
+                if (s != null)
+                {
+                    var ss = s.Split(',');
+                    if (ss.Count() > 4)
+                        if (ss[4].Length > 3) result.WriteLine(ss[4].Substring(1, ss[4].Length - 2));
+                }
+            }
+            result.Close();
         }
     }
 }
